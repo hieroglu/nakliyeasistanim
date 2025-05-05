598362d52a774b79e6c9079203775966f574c316
Halil İbrahim EROĞLU <h.ibrahimeroglu@outlook.com.tr>
2025-05-05 04:05:54 +0300

    Initial project setup with Django backend and React frontend

--- nakliyeasistanim_backend/nakliyeasistanim_backend/settings.py
+++ nakliyeasistanim_backend/nakliyeasistanim_backend/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'nakliyeasistanim_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nakliye_db',          # pgAdmin'de oluşturduğunuz veritabanı adı
+        'USER': 'nakliye_user',        # pgAdmin'de oluşturduğunuz kullanıcı adı
+        'PASSWORD': 'root',  # pgAdmin'de belirlediğiniz şifre
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
